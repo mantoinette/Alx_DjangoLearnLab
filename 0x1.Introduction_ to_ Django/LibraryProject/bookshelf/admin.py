34ab2dce8ef5b47e80ef8eacb6154133a09ee238
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-08-23 00:21:23 +0200

    Update admin.py

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-
 from .models import Book
-admin.site.register(Book)
+from django.utils import timezone
+
+
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
+    list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
+# Register your models here.
+#admin.site.register(Book)
