419bc72c52be83189a0edfd48e2bec5a00f60050
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-08-29 22:54:50 +0200

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,15 +1,23 @@
-# views.py
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
+# Create your views here.
 def book_list(request):
     """Retrieves all books and renders a template displaying the list."""
-    books = Book.objects.all()
+    books = Book.objects.all()  # Fetch all book instances from the database
     context = {'book_list': books}
     return render(request, 'relationship_app/list_books.html', context)
+
+class library_list(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
 
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+
+def list_books(request):
+    # Your view logic here
+    return render(request, 'relationship_app/list_books.html')
