a9575bdf6fd23b0b368f0f3d37abb79444d974a3
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-08-29 11:26:21 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,26 +1,32 @@
 from django.db import models
 
-# Create your models here.
-
+# Author Model
 class Author(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
+# Book Model
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete= models.CASCADE)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
     def __str__(self):
         return self.title
 
+# Library Model
 class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
     def __str__(self):
         return self.name
 
+# Librarian Model
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
     def __str__(self):
         return self.name
