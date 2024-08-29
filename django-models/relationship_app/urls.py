e79629d48d91348e0001cd42f641b7088ca6f76b
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-08-30 00:31:03 +0200

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from .views import list_books
-from .views import book_list, LibraryDetailView
-from .views import user_login, user_logout, user_register
+from .views import book_list, LibraryDetailView, user_login, user_logout, user_register
 
 urlpatterns = [
     path('books/', book_list, name='list_books'),  # Function-based view to list books
@@ -9,5 +7,6 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('register/', user_register, name='register'),
+]
 
 ]
