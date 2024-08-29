23f780a83bbe532c6abe25acb549508224aff793
jdam-github <jdam600@gmail.com>
2024-08-29 15:18:41 +0200

    put the filter object filter in the function of get book  by particular author

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,9 +2,13 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def get_books_by_author(author_name):
+    # Get the author object by name
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    
+    # Retrieve all books associated with this author
+    books = Book.objects.filter(author=author)
     return books
+
 
 # List all books in a library
 def get_books_in_library(library_name):
