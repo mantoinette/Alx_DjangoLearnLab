938ce864c680e67abbc097c651d2d246a4c7f304
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-08-30 17:49:19 +0200

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -3,14 +3,12 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Author Model
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
-# Book Model
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
@@ -18,7 +16,6 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-# Library Model
 class Library(models.Model):
     name = models.CharField(max_length=255)
     books = models.ManyToManyField(Book, related_name='libraries')
@@ -26,7 +23,6 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
-# Librarian Model
 class Librarian(models.Model):
     name = models.CharField(max_length=255)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
@@ -34,21 +30,18 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
-# UserProfile Model
 class UserProfile(models.Model):
     ROLE_CHOICES = (
         ('Admin', 'Admin'),
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     )
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-# Signal to create or update UserProfile when User is created or updated
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
