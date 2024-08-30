381adcd1768510d0d2ea330788cd7a5ec763f4b2
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-08-30 15:16:21 +0200

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,18 +3,21 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.views.generic.detail import DetailView
-from .models import Book, Library
 from django.contrib.auth.decorators import user_passes_test  # Import this
+from .models import Book, Library
 
+# List all books
 def book_list(request):
     books = Book.objects.all()
     context = {'book_list': books}
     return render(request, 'relationship_app/list_books.html', context)
 
+# Detail view for a library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
 
+# Login view
 def user_login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
@@ -26,10 +29,12 @@ def user_login(request):
         form = AuthenticationForm()
     return render(request, 'relationship_app/login.html', {'form': form})
 
+# Logout view
 def user_logout(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
 
+# Registration view
 def user_register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -43,23 +48,29 @@ def user_register(request):
 
 # Role-Based Access Control Views
 
+# Check if the user is an Admin
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
+# Check if the user is a Librarian
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
 
+# Check if the user is a Member
 def is_member(user):
     return user.userprofile.role == 'Member'
 
+# Admin view, accessible only by Admin users
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')  # Adjust the template path as needed
 
+# Librarian view, accessible only by Librarian users
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')  # Adjust the template path as needed
 
+# Member view, accessible only by Member users
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')  # Adjust the template path as needed
