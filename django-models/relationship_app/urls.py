86bbc258b41073efdf255daafdc856d0590b6d10
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-08-30 17:08:45 +0200

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,11 +4,14 @@ from .views import admin_view, librarian_view, member_view, book_list, LibraryDe
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', views.book_list, name='home'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.user_register, name='register'),
+    path('', views.book_list_view, name='home'),
+    path('', views.LibraryDetails_view, name='home'),
+    "LogoutView.as_view(template_name="logut.html, 
+    "LoginView.as_view(template_name="login.html"
+    "views.register"
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
