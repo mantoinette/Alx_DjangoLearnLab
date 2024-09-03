8b563a8116fec9cb0484ebcf1aef7df626e81e38
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-03 16:35:14 +0200

    Update urls.py

--- api_project/api_project/api/urls.py
+++ api_project/api_project/api/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+# api/urls.py
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include the app's URLs
+    path('books/', views.BookListCreateView.as_view(), name='book-list-create'),
+    # Add more paths as needed
 ]
