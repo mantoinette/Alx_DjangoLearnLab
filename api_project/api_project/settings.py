f34bee614cc4534cf2792259312d6f668d1cd51d
jdam-github <jdam600@gmail.com>
2024-09-03 21:56:34 +0200

    rename api_project to api_projects

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api', 
-    'rest_framework',
+    'api',  # Ensure this is added
+    'rest_framework',  # Ensure this is added
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
