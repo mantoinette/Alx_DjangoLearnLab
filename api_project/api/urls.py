739418a8c7c87fcac14226404821f9c684ab0afc
jdam-github <jdam600@gmail.com>
2024-09-04 12:24:13 +0200

    create bookserializer in order to do all CRUDE

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,12 @@
-# api/urls.py
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
 
+# Create a router and register the BookViewSet
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+# Include the router URLs in your urlpatterns
 urlpatterns = [
-  path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
