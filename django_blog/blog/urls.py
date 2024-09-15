57dae52107db8bedecbd8dfa82890ab89034cb42
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-15 22:56:26 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
     path('edit_profile/', edit_profile, name='edit_profile' ),
-    path('post/new/', CreateView.as_view(), name='new'),
-    path('posts/', ListView.as_view(), name='posts'),
-    path('post/<int:pk>', DetailView.as_view(), name='detail'),
-    path('post/<int:pk>/delete/', DeleteView.as_view(), name='delete'),
-    path('post/<int:pk>/update/', UpdateView.as_view(), name='update'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
 ]
