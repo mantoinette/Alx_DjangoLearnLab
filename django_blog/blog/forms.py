ac3ff5714a21a46f01bb7a96ace32e48f6f8a841
jdam-github <jdam600@gmail.com>
2024-09-15 21:15:44 +0200

    debuging the errors

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,10 +1,22 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from .models import Profile, Post
 
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
+class ProfileForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = Profile
+        fields = ['email', 'bio', 'picture']
+
+class CreatePostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+
+class UpdatePostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'})
+
+        }
