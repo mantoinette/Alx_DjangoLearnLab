977c3ed71a5c095522e31f56185d757acbbde7fc
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-15 22:46:37 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -8,6 +8,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import CustomUser, Post
 from .forms import ProfileForm, CreatePostForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -79,3 +83,104 @@ class DeleteView(UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return post.author == self.request.user
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # Template for displaying all posts
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # Display latest posts first
+
+# Display a single blog post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'  # Template for displaying a single post
+
+# Allow users to create a new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Set the author to the logged-in user
+        return super().form_valid(form)
+
+# Allow post authors to update their posts
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Ensure the author is the logged-in user
+        return super().form_valid(form)
+
+    # Ensure only the post's author can update it
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Allow post authors to delete their posts
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post-list')  # Redirect to the post list after deletion
+    template_name = 'blog/post_confirm_delete.html'
+
+    # Ensure only the post's author can delete it
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+Step 2: Create and Configure Forms
+For creating and updating blog posts, you will use Django’s ModelForm to handle form validation and rendering.
+
+You don’t need to explicitly define a form class, as Django's class-based CreateView and UpdateView will automatically use a ModelForm if you provide the model and fields. The form will automatically validate input.
+
+Step 3: Set Up Templates for Each Operation
+Create the following templates in the blog/templates/blog/ directory:
+
+3.1 Template for listing posts (post_list.html):
+html
+Copy code
+{% extends "blog/base.html" %}
+
+{% block content %}
+  <h2>All Blog Posts</h2>
+  {% for post in posts %}
+    <div>
+      <h3><a href="{% url 'post-detail' post.pk %}">{{ post.title }}</a></h3>
+      <p>By {{ post.author }} on {{ post.date_posted }}</p>
+      <p>{{ post.content|truncatewords:30 }}</p>
+    </div>
+  {% endfor %}
+{% endblock %}
+3.2 Template for viewing a single post (post_detail.html):
+html
+Copy code
+{% extends "blog/base.html" %}
+
+{% block content %}
+  <article>
+    <h2>{{ post.title }}</h2>
+    <p>By {{ post.author }} on {{ post.date_posted }}</p>
+    <p>{{ post.content }}</p>
+
+    {% if user == post.author %}
+      <a href="{% url 'post-update' post.pk %}">Edit</a>
+      <a href="{% url 'post-delete' post.pk %}">Delete</a>
+    {% endif %}
+  </article>
+{% endblock %}
+3.3 Template for creating/editing posts (post_form.html):
+html
+Copy code
+{% extends "blog/base.html" %}
+
+{% block content %}
+  <h2>{% if form.instance.pk %}Edit Post{% else %}Create New Post{% endif %}</h2>
+  <form method="POST">
+    {% csrf_token %}
+    {{ form.as_p }}
+    <button type="submit">{% if form.instance.pk %}Update{% else %}Create{% endif %}</button>
+  </form>
+{% endblock %}
