b17a2e75366cf23b6aa13ad48eb9dbd84c0bfe49
jdam-github <jdam600@gmail.com>
2024-09-17 14:02:42 +0200

    working on the tag files

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -26,3 +26,8 @@ class CustomUser(AbstractUser):
     username = models.CharField(unique=True, max_length=100)
     email = None
      
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
