42dbae6384981b398b05a7a1a2851c20318f4c1c
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-17 13:40:53 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile, Post
+from .models import Profile, Post, Comment  # Import the Comment model
 from django.contrib.auth.models import User
 
 class ProfileForm(forms.ModelForm):
@@ -17,12 +17,20 @@ class UpdatePostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content']
         widgets = {
-            'title':forms.TextInput(attrs={'class':'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control'})
-
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+# Add CommentForm
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment  # Specify the Comment model
+        fields = ['content']  # Allow users to input comment content
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
