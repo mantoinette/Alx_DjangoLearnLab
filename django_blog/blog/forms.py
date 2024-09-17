b1d9d539dd19d62351f0a0a8c73f3de264bacd2d
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-17 14:14:50 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,41 +1,47 @@
 from django import forms
-from taggit.forms import TagWidget
-from taggit.utils import slugify
-from .models import Profile, Post, Comment, Tag
-
-
+from .models import Profile, Post
+from django.contrib.auth.models import User
+from django import Tag, Post
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('email', 'bio', 'picture')
-
+        fields = ['email', 'bio', 'picture']
 class CreatePostForm(forms.ModelForm):
-    tags = forms.CharField(required=False, widget=TagWidget())
-
     class Meta:
         model = Post
-        fields = ('title', 'content')
+        fields = ['title', 'content', 'tags']
         widgets = {
-            'tags': TagWidget
+            'tags': TagWidget(),  # Tag input widget
         }
-
-    def clean_tags(self):
-        tags = self.cleaned_data['tags']
-        existing_tags = Tag.objects.filter(name__in=[slugify(tags) for tag in tags.split(',')])
-        new_tags = [tag for tag in tags.split(',') if slugify(tag) not in existing_tags.values_list('name', flat=True)]
-        return ', '.join(existing_tags.values_list('name', flat=True) + new_tags)
 
 class UpdatePostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'content')
+        fields = ['title', 'content']
         widgets = {
             'title':forms.TextInput(attrs={'class':'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'})
 
+
         }
 
-class CommentForm(forms.ModelForm):
+class ProfileForm(forms.ModelForm):
     class Meta:
-        model = Comment
-        fields = ('content',)
+        model = User
+        fields = ['username', 'email']
+
+class PostForm(forms.ModelForm):
+    tags = forms.CharField()  # Add this line to handle tags as a comma-separated string
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        tag_names = self.cleaned_data['tags'].split(',')  # Split by commas
+        tags = [Tag.objects.get_or_create(name=tag_name.strip())[0] for tag_name in tag_names]
+        if commit:
+            instance.save()
+            instance.tags.set(tags)
+        return instance
