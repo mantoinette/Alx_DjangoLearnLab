cbb1578cc52190e5609dfefee8442f6179327eda
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-17 14:10:38 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,13 +6,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import CustomUser, Post
-from .forms import ProfileForm, CreatePostForm
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
-from .models import Comment
+from .models import CustomUser, Post, Comment
+from .forms import ProfileForm, CreatePostForm, CommentForm
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -46,18 +41,14 @@ def edit_profile(request):
     return render(request, 'edit_profile.html', {'form': form})
 
 
- # blog/views.py (Update profile view)
+
 @login_required
 def profile(request):
-    if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')
-    else:
-        form = ProfileForm(instance=request.user)
-    return render(request, 'registration/profile.html', {'form': form})
+    return render(request, 'profile.html', {'user': request.user})
 
+class ListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
 
 class DetailView(DetailView):
     model = Post
@@ -89,54 +80,7 @@ class DeleteView(UserPassesTestMixin, DeleteView):
         post = self.get_object()
         return post.author == self.request.user
 
-
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'  # Template for displaying all posts
-    context_object_name = 'posts'
-    ordering = ['-date_posted']  # Display latest posts first
-
-# Display a single blog post
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'  # Template for displaying a single post
-
-# Allow users to create a new post
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    fields = ['title', 'content']
-    template_name = 'blog/post_form.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user  # Set the author to the logged-in user
-        return super().form_valid(form)
-
-# Allow post authors to update their posts
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    fields = ['title', 'content']
-    template_name = 'blog/post_form.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user  # Ensure the author is the logged-in user
-        return super().form_valid(form)
-
-    # Ensure only the post's author can update it
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-
-# Allow post authors to delete their posts
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    success_url = reverse_lazy('post-list')  # Redirect to the post list after deletion
-    template_name = 'blog/post_confirm_delete.html'
-
-    # Ensure only the post's author can delete it
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-    class CommentCreateView(CreateView):
+class CommentCreateView(CreateView):
     model = Comment
     template_name = 'comment_create.html'
     form_class = CommentForm
@@ -152,5 +96,3 @@ class CommentUpdateView(UpdateView):
 class CommentDeleteView(DeleteView):
     model = Comment
     template_name = 'comment_delete.html'
-
-
