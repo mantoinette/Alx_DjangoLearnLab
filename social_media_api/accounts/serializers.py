e14efe18dd462dfdb6f87cd960f080c6c52d279b
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-19 00:18:48 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,44 +1,22 @@
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
 from .models import CustomUser
 
-# Serializer for the CustomUser model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
-# Serializer for user registration
 class RegisterSerializer(serializers.ModelSerializer):
-    # Use serializers.CharField for password and confirm_password
-    password = serializers.CharField(write_only=True, required=True)
-    confirm_password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password', 'confirm_password']
-        extra_kwargs = {
-            'password': {'write_only': True},
-            'confirm_password': {'write_only': True},
-        }
+        fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        confirm_password = validated_data.pop('confirm_password')
-
-        # Ensure the passwords match
-        if password != confirm_password:
-            raise serializers.ValidationError({"password": "Passwords must match."})
-
-        # Create the user
-        user = get_user_model().objects.create_user(
+        user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=password
+            password=validated_data['password']
         )
-
-        # Create the token for the user
-        Token.objects.create(user=user)
-        
         return user
