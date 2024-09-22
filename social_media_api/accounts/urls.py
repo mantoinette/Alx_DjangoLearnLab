f2e7b499202fdfd11999877fd76e1523092e75d5
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-22 18:39:47 +0200

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomAuthToken.as_view(), name='login'),
 
-    # Custom paths for follow and unfollow actions, using user_id instead of pk
-    path('users/<int:user_id>/follow/', FollowViewSet.as_view({'post': 'follow'}), name='follow-user'),
-    path('users/<int:user_id>/unfollow/', FollowViewSet.as_view({'post': 'unfollow'}), name='unfollow-user'),
+    # Custom paths for follow and unfollow actions using user_id
+    path('follow/<int:user_id>/', FollowViewSet.as_view({'post': 'follow'}), name='follow-user'),
+    path('unfollow/<int:user_id>/', FollowViewSet.as_view({'post': 'unfollow'}), name='unfollow-user'),
 
-    # Include all router-generated URLs (for standard CRUD actions)
+    # Include all router-generated URLs for standard actions
     path('', include(router.urls)),
 ]
