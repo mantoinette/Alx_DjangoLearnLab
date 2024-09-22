789d29b5c1d0c46c2a0209be142c014c4dfeefc4
jdam-github <jdam600@gmail.com>
2024-09-22 18:13:29 +0200

    adding same staff

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,15 +1,16 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.views import APIView
 from .models import CustomUser
 from .serializers import UserSerializer, RegisterSerializer
 
+# Registration view for creating new users
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
 
+# Custom token authentication view
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={'request': request})
@@ -21,3 +22,9 @@ class CustomAuthToken(ObtainAuthToken):
             'user_id': user.pk,
             'username': user.username
         })
+
+# Example of a view that requires authentication
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Authentication required
