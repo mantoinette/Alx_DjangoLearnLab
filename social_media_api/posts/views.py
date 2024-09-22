bf557e5899f89d3910374328f78e7585d292389a
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-23 00:22:30 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -61,5 +61,3 @@ class PostViewSet(viewsets.ModelViewSet):
         posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serializer = self.get_serializer(posts, many=True)
         return Response(serializer.data)
-
-# You would also need to define the CommentViewSet and other necessary views here.
