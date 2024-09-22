53d78050a4c8ead921d5ae84f74d4d44a4623530
jdam-github <jdam600@gmail.com>
2024-09-22 17:55:28 +0200

    working on router for viewsets

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+    
+    # Followers and Following Relationships
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='user_followers', blank=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='user_following', blank=True)
 
     def __str__(self):
         return self.username
