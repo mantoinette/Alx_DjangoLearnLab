65adb77188eed78caf8d1632695f0a66bd2b6d6a
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-22 18:25:19 +0200

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import RegisterView, CustomAuthToken
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import RegisterView, CustomAuthToken, FollowViewSet
+
+router = DefaultRouter()
+router.register(r'users', FollowViewSet, basename='user')
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomAuthToken.as_view(), name='login'),
+    path('', include(router.urls)),
 ]
