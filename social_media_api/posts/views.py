2d7c059d569dde5334416cb0edfac88045f32c20
mantoinette <152092675+mantoinette@users.noreply.github.com>
2024-09-25 13:27:50 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -43,33 +43,3 @@ def unlike_post(request, pk):
         return Response({'message': 'Post unliked'}, status=status.HTTP_204_NO_CONTENT)
     else:
         return Response({'error': 'Post not liked yet'}, status=status.HTTP_400_BAD_REQUEST)
-
-# ViewSet for handling posts
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')  # Get all posts ordered by creation date
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
-
-    def perform_create(self, serializer):
-        # Automatically associate the post with the user making the request
-        serializer.save(author=self.request.user)
-
-    # Custom feed action to get posts from followed users
-    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
-    def feed(self, request):
-        following_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
-        serializer = self.get_serializer(posts, many=True)
-        return Response(serializer.data)
-
-# ViewSet for handling comments
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        # Automatically associate the comment with the user making the request
-        serializer.save(author=self.request.user)
